3e07490039944faca609c8e22b4157ed6e1ed137
jcsgasche <gasche@protonmail.com>
2023-12-12 00:17:48 +0100

    aesthetic adjustments

--- jla.py
+++ jla.py
@@ -11,46 +11,30 @@ def mina(a, b, c):
 def mina2(a, b, c):
     if b**2 < (4*a*c):
         return('Nicht definiert, weil negative Diskriminante')
-
+    
     wurzelteil = 'sqrt({})'.format(b**2 - 4*a*c)
     nenner = '2*{}'.format(a)
-
     l1 = '(-{} + {}) / {}'.format(b, wurzelteil, nenner)
     l2 = '(-{} - {}) / {}'.format(b, wurzelteil, nenner)
+    
     return(l1, l2)
 
 def pq(x, y, z): 
-
     discri = y * y - 4 * x * z 
-
     sqrtval = math.sqrt(abs(discri)) 
 
     # checking condition for discriminant
-
     if discri > 0: 
-
-        print(" real and different roots ") 
-
+        print("real and different roots:") 
         print((-y + sqrtval)/(2 * x)) 
-
         print((-y - sqrtval)/(2 * x)) 
 
-      
-
-    elif discri == 0: 
-
-        print(" real and same roots") 
-
+    elif discri == 0:
+        print("real and same roots:") 
         print(-y / (2 * x)) 
 
-      
-
     # when discriminant is less than 0
-
     else:
-
         print("Complex Roots") 
-
         print(- y / (2 * x), " + i", sqrtval) 
-
         print(- y / (2 * x), " - i", sqrtval)
