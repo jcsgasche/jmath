29b5bddde28aa88910de014d0fd08d1bc6c5f160
Joshua <132779387+jcsgasche@users.noreply.github.com>
2023-12-15 09:44:42 +0100

    Update jla.py

--- jla.py
+++ jla.py
@@ -2,7 +2,7 @@ import math
 
 def mina(a, b, c):
 	if b**2 < (4*a*c):
-		return('Nicht definiert, weil negative Diskriminante')
+		return('Not defined because negative discriminant')
 
 	l1 = (-b + (b**2 - (4*a*c))**(0.5)) / (2*a)
 	l2 = (-b - (b**2 - (4*a*c))**(0.5)) / (2*a)
@@ -10,7 +10,7 @@ def mina(a, b, c):
 
 def mina2(a, b, c):
     if b**2 < (4*a*c):
-        return('Nicht definiert, weil negative Diskriminante')
+        return('Not defined because negative discriminant')
     
     wurzelteil = 'sqrt({})'.format(b**2 - 4*a*c)
     nenner = '2*{}'.format(a)
